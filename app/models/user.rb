803c9ac321c40aa88ab478eba4d9c8d51f17fabe
kaviyakumar23 <kaviyakumar23@gmail.com>
2025-03-17 11:58:51 +0530

    Add email validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,8 @@ class User < ApplicationRecord
     has_one :event_organizer, dependent: :destroy
     has_one :customer, dependent: :destroy
 
-    validates :email, presence: true, uniqueness: true
+    validates :email, presence: true, uniqueness: true, 
+                     format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i, message: "must be a valid email address" }
     validates :password, presence: true, length: { minimum: 8 }
     validates :role, presence: true, inclusion: { in: ['event_organizer', 'customer'] }
 end
